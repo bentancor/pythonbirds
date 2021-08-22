4c8f0c3a5f1a362fadf439e32654fc435f5e4f6c
Marco <marco.bentancor@gmail.com>
2021-08-21 22:18:42 -0300

    Criado tributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -8,6 +8,14 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
 if __name__ == '__main__':
     raquelly = Pessoa(nome='Raquelly')
     marco = Pessoa(raquelly, nome='Marco')
@@ -28,4 +36,5 @@ if __name__ == '__main__':
     print(marco.olhos)
     print(raquelly.olhos)
     print(id(Pessoa.olhos), id(marco.olhos), id(raquelly.olhos))
-
+    print(Pessoa.metodo_estatico(), marco.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), marco.nome_e_atributos_de_classe())
